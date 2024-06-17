393049ded2184f8e1e17ae5be8721236ee3bbca8
Guigui66IUT <g.juan@etudiant.univ-perp.fr>
2024-06-17 17:50:19 +0200

    Votre message de commit

--- javascript/header.js
+++ javascript/header.js
@@ -2,53 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch('/html/header.html')
         .then(response => response.text())
         .then(data => {
-            document.getElementById('header').innerHTML = data;
-            adjustPaths();
-            setActiveClass();
-        });
-
-    function adjustPaths() {
-        const header = document.getElementById('header');
-        const links = header.querySelectorAll('a');
-        const depth = window.location.pathname.split('/').filter(part => part).length - 1;
-
-        links.forEach(link => {
-            let href = link.getAttribute('href');
-            if (href.startsWith('/')) {
-                href = href.substring(1);
-                let path = '';
-                for (let i = 0; i < depth; i++) {
-                    path += '../';
-                }
-                path += href;
-                link.setAttribute('href', path);
-            }
-        });
-    }
-
-    function setActiveClass() {
-        const links = document.querySelectorAll('nav a');
-        const currentPath = window.location.pathname;
-
-        // Variable pour suivre si un lien actif a été défini
-        let activeLinkSet = false;
-
-        links.forEach(link => {
-            const linkPath = new URL(link.href).pathname;
-            if (!activeLinkSet && (linkPath === currentPath || (currentPath === '/' && linkPath === '/index.html'))) {
-                link.classList.add('active');
-                activeLinkSet = true;
-            } else {
-                link.classList.remove('active');
-            }
-        });
-
-        // Ajoute la classe active au lien "Professionnels de santé" si on est dans une de ses pages
-        if (currentPath.includes('/html/profession/')) {
-            const dropdownToggle = document.querySelector('a.dropdown-toggle');
-            if (dropdownToggle) {
-                dropdownToggle.classList.add('active');
-            }
-        }
-    }
+            document.getElementById('main-header').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading header:', error));
 });
