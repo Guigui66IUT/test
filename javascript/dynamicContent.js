f2d137403b1059a199056e68666a07f966baf494
Guigui66IUT <g.juan@etudiant.univ-perp.fr>
2024-06-25 14:51:24 +0200

    Votre message de commit

--- javascript/dynamicContent.js
+++ javascript/dynamicContent.js
@@ -6,8 +6,24 @@ document.addEventListener("DOMContentLoaded", function() {
         const professionLogo = document.getElementById('profession-logo');
         const professionTitle = document.getElementById('profession-title');
 
-        professionLogo.src = `/ajoutprofession/${profession}/logo-${profession}.png`;
-        professionLogo.alt = `Logo ${profession.charAt(0).toUpperCase() + profession.slice(1)}`;
+        // Chemin du logo
+        const logoPath = `/ajoutprofession/${profession}/logo-${profession}.png`;
+
+        // Vérifier l'existence du logo
+        fetch(logoPath)
+            .then(response => {
+                if (response.ok) {
+                    professionLogo.src = logoPath;
+                    professionLogo.alt = `Logo ${profession.charAt(0).toUpperCase() + profession.slice(1)}`;
+                    professionLogo.style.display = 'block';
+                } else {
+                    professionLogo.style.display = 'none';
+                }
+            })
+            .catch(() => {
+                professionLogo.style.display = 'none';
+            });
+
         professionTitle.textContent = profession.charAt(0).toUpperCase() + profession.slice(1);
     }
 });
