0c4e4ecd6d1554f9cf2885a607b57a05ed0e8810
Guigui66IUT <g.juan@etudiant.univ-perp.fr>
2024-08-08 17:12:09 +0200

    Votre message de commit

--- python/generate_JSONlist_actu.py
+++ python/generate_JSONlist_actu.py
@@ -1,5 +1,6 @@
 import os
 import json
+from datetime import datetime
 
 def generate_JSONlist_actu(directory=None, output=None):
     # Obtenir le répertoire du script en cours d'exécution
@@ -9,9 +10,23 @@ def generate_JSONlist_actu(directory=None, output=None):
     directory = directory or os.path.join(script_directory, '../pdf')
     output = output or os.path.join(script_directory, '../json/file_list.json')
 
-    # Récupérer tous les fichiers PDF dans le répertoire spécifié
-    pdf_files = [f.replace('.pdf', '') for f in os.listdir(directory) if f.endswith('.pdf')]
-    
+    # Initialiser la structure de données pour les fichiers PDF
+    pdf_files = {'toujours': []}
+    always_dir = os.path.join(directory, 'toujours')
+
+    # Ajouter les fichiers du dossier "toujours"
+    if os.path.exists(always_dir):
+        pdf_files['toujours'] = [f.replace('.pdf', '') for f in os.listdir(always_dir) if f.endswith('.pdf')]
+
+    # Obtenir le mois actuel
+    current_month = datetime.now().strftime('%B').lower()
+
+    # Parcourir tous les dossiers dans le répertoire spécifié
+    for subdir in os.listdir(directory):
+        subdir_path = os.path.join(directory, subdir)
+        if os.path.isdir(subdir_path) and subdir.lower() == current_month:
+            pdf_files[subdir] = [f.replace('.pdf', '') for f in os.listdir(subdir_path) if f.endswith('.pdf')]
+
     # Écrire la liste des fichiers dans un fichier JSON
     with open(output, 'w') as json_file:
         json.dump(pdf_files, json_file, indent=4)
